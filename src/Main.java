72c3605e6f9834f53a0303fbc2894ca98206ff3f
jerrraa <jeromegould2002@gmail.com>
2024-11-27 07:43:25 -0400

    forgot to add some files, whoops.

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import javax.swing.*;
+
+
+public class Main {
+// Made by Jerra Gould / JERRAqt 2024.
+
+    public static void main(String[] args) {
+        MusicPlayer setupplayer = new MusicPlayer();
+        setupplayer.setupGUI();
+    }
+}
